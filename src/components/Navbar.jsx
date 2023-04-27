ace07f46a8caa1ca8e619f730014ad5eebcf5f77
Helina Ashenafi <Consci210@gmail.com>
2023-04-27 11:15:39 +0300

    created home and main

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <div>
                 <button className='text-white pr-4'>Sign In</button>
                 <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>
-                  Sign Up
+                  Sign USp
                 </button>
             </div>
         </div>
